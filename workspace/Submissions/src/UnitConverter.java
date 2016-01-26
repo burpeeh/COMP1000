808f2ebd9accf91c1abe44d14a50c33fdb962c85
Howard Burpee <burpeeh@wit.edu>
2016-01-26 10:16:10 -0500

    Work in progress.

--- workspace/Submissions/src/UnitConverter.java
+++ workspace/Submissions/src/UnitConverter.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 
 public class UnitConverter {
-	public static void main(String[] args) {
+	
+	//Function for Temperature calculations
+	public static double Temp(double F, Scanner scanner) {
+		System.out.print("Enter a value in degrees Farenheit..\n");
+		F = scanner.nextDouble();
+		
+		double C = (F-32)*(5.0/9);
+		double K = (F-32)*(5.0/9)+273.15;
+		
+		return C;
+		
+		
 		
 	}
-}
+	
+	//Main -- inputs
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double temp1, temp2;
+		
+		System.out.println("What converter would you like to use?\n - temperature\n" +
+		" - mass\n - distance\n - time\n");
+		String ans = scanner.nextLine();
+		
+		if (ans.equals("temperature")) {
+			System.out.format("Enter a val");
+			
+			
+		}
+		
+	}
+} 
